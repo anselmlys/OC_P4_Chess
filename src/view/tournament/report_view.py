7102622d1dd22dfe746ba3a12328dbc894e6009a
anselmlys <anselm.lisa@gmail.com>
2025-10-14 15:05:13 +0200

    MAJ affichage plus complet des informations d'un tournoi

--- src/view/tournament/report_view.py
+++ src/view/tournament/report_view.py
@@ -15,7 +15,11 @@ class TournamentReportView:
         input("\nPress Enter to continue...\n")
 
     def tournament_info(self, tournament):
-        print("\nVoici l'ensemble des tours et de leurs matchs :")
+        print((f"\nNom : {tournament.name.title()}\n"
+               f"Lieu : {tournament.place.title()}\n"
+               f"Date de début : {tournament.start_date}\n"
+               f"Date de fin : {tournament.end_date}\n"))
+        print("Voici l'ensemble des tours et de leurs matchs :")
         for round in tournament.rounds:
             print((f"\nNom : {round.name}\n"
                    f"Date de début : {round.start_datetime}\n"
